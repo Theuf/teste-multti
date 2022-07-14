29380d3482c9557fd5b5fc104fb0224dd78c6221
Theuf <matheus13_felipe@hotmail.com>
2022-07-14 11:43:01 -0300

    att

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,9 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      "viewportWidth"; 1333,
+      "viewportHeight"; 660,
+      "baseurl"; "https://app-hml.contabilhub.com.br/home"
     },
   },
 });
