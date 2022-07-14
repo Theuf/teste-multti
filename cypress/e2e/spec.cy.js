a3b4f0d039d83961653ba6195a1a610bfa7eba14
Theuf <matheus13_felipe@hotmail.com>
2022-07-14 11:11:20 -0300

    teste-multti

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,31 @@
+
+
+describe('home page', ()=>{
+  
+  it('realizar login', ()=>{
+    cy.viewport(1366, 660)
+    cy.visit('https://app-hml.contabilhub.com.br/home')
+    cy.get('p[class="login-description-page"]').should('have.text','Acesso a plataforma')
+    cy.get('input[placeholder="seu e-mail"]').type('usuariotestes@multti.com')
+    cy.get('input[placeholder="sua senha"]').type('123456')
+    cy.get('.login-button').click()
+  })
+
+  it('filtragem nfs', ()=>{
+    cy.viewport(1366, 660)
+      cy.visit('https://app-hml.contabilhub.com.br/home')
+      cy.get('p[class="login-description-page"]').should('have.text','Acesso a plataforma')
+      cy.get('input[placeholder="seu e-mail"]').type('usuariotestes@multti.com')
+      cy.get('input[placeholder="sua senha"]').type('123456')
+      cy.get('.login-button').click().wait(5000)
+      cy.get(':nth-child(3) > .navbar-item-folder-vertical').click()
+      cy.get(' div:nth-child(3) > ul > li:nth-child(1) > a > span').click()
+      cy.get(' div.styles_formSelectInput__1wy97.css-2b097c-container > div').type('ADRIANA WAGNER KARPAT{enter}')
+      cy.get(':nth-child(2) > .calendar-container > .react-datepicker-wrapper > .react-datepicker__input-container > .input-field').clear().type("01042022")
+      cy.get(':nth-child(3) > .calendar-container > .react-datepicker-wrapper > .react-datepicker__input-container > .input-field').clear().type('13072022')
+      cy.get('.filter-button-area > .btn-primary').click()
+      cy.get('.moreoptionsdisplay-wrapper > .icon-wrapper').click()
+      cy.get('.modal-options-list-item').click()
+
+  })
+})
